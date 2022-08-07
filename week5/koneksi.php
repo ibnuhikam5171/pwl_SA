d8ac488a07febcfff4f1ecbbc3648a72ebbd6273
Badai Hikam I.A <81012355+ibnuhikam5171@users.noreply.github.com>
2022-08-07 18:28:13 +0700

    Add files via upload

---
+++ week5/koneksi.php
@@ -0,0 +1,13 @@
+<?php 
+$host="localhost:3307";
+$user="root";
+$password="";
+$db="shopping_cart";
+
+$kon = mysqli_connect($host,$user,$password,$db);
+
+if(!$kon){
+	die("Koneksi gagal:".mysqli_connect_error());
+ }
+
+?>
